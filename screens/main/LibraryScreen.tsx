359f7a26ce073f71ea4c679482cc23d900963387
Bug0427 <Bug0427@users.noreply.github.com>
2026-08-10 14:41:36 +0000

    fix: remove duplicate useSortPreference import in LibraryScreen

--- screens/main/LibraryScreen.tsx
+++ screens/main/LibraryScreen.tsx
@@ -47,9 +47,6 @@ import { getCollections, type Collection } from '../../services/collectionServic
 // Sort
 import { useSortPreference, applySortOrder } from '../../hooks/useSortPreference';
 
-// Sort
-import { useSortPreference, applySortOrder } from '../../hooks/useSortPreference';
-
 // Filters
 import { FilterState, DEFAULT_FILTER_STATE, hasActiveFilters } from '../../utils/filters';
 
